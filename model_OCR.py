547a26599c0b1138cfeaa84f84199796dc51fbe4
sebastiandsc <sdsanchez@uninorte.edu.co>
2021-11-30 17:45:05 -0400

    EvaluateModel Added

--- model_OCR.py
+++ model_OCR.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 29 16:21:36 2021
-
-@author: Sebastian
+ICOM 6015
+@author: Sebastian Sanchez
 """
 
 import os
@@ -11,7 +10,10 @@ from tensorflow.keras import optimizers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dropout, Flatten, Dense
 from tensorflow.keras.layers import  Convolution2D, MaxPooling2D
+from sklearn.metrics import classification_report, confusion_matrix
 from tensorflow.keras import backend as K
+import numpy as np
+import matplotlib.pyplot as plt
 
 K.clear_session()
     
@@ -30,14 +32,14 @@ Val_dataGen = ImageDataGenerator(
     rescale=1./255   
     )
 
-imagen_entrenamiento = Train_dataGen.flow_from_directory(
+train_data_img = Train_dataGen.flow_from_directory(
     Train_Data,
     target_size = (28,28),
     batch_size = 32,
     class_mode = 'categorical'
     )
 
-imagen_validacion = Val_dataGen.flow_from_directory(
+val_data_img = Val_dataGen.flow_from_directory(
     Val_data,
     target_size = (28,28),
     batch_size = 32,
@@ -71,13 +73,14 @@ Model.add(MaxPooling2D(pool_size=(2,2)))
 
 Model.add(Flatten())
 
-Model.add(Dense(256, activation = 'tanh'))
+Model.add(Dense(128, activation = 'tanh'))
 Model.add(Dropout(0.4))
 Model.add(Dense(256, activation='relu'))
 Model.add(Dropout(rate = 0.4))
-Model.add(Dense(256, activation='relu'))
+Model.add(Dense(512, activation='relu'))
 Model.add(Dropout(rate = 0.4))
-
+Model.add(Dense(128, activation = 'tanh'))
+Model.add(Dropout(0.4))
 Model.add(Dense(classes, activation='softmax'))
 
 Model.summary()
@@ -85,9 +88,35 @@ Model.summary()
 
 Model.compile(loss='categorical_crossentropy',optimizer=optimizers.Adam(lr=0.0001),metrics=('accuracy'))
 
-History = Model.fit_generator(imagen_entrenamiento, epochs = 80, 
+History = Model.fit_generator(train_data_img, epochs = 120, 
                     steps_per_epoch = 864//32, 
-                    validation_data=imagen_validacion)
+                    validation_data=val_data_img)
+
+scores = Model.evaluate(val_data_img)
+print("%s%s: %.2f%%" % ("evaluate_generator ",Model.metrics_names[1], scores[1]*100))
+
+#Confution Matrix and Classification Report
+Y_pred = Model.predict(val_data_img, 864//33)
+y_pred = np.argmax(Y_pred, axis=1)
+
+
+Matrix_C = confusion_matrix(val_data_img.classes, y_pred)
+
+target_names = [] 
+for x in range(0,36): target_names.append(str(x))
+
+Class_Report = classification_report(val_data_img.classes, y_pred, target_names=target_names)
+print(Class_Report)
+
+plt.plot(History.history["loss"], label="train_loss")
+plt.plot(History.history["val_loss"], label="val_loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.title("Train and Validation Losses Over Epochs", fontsize=14)
+plt.legend()
+plt.grid()
+plt.show()
+
 
 dir = 'C:/Users/Sebastian/Desktop/REDES NEURONALES'
 if not os.path.exists(dir):
